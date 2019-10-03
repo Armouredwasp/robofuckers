0761f9cf294cd6deaf38b8a6b9eb6b231ceb5bf8
Armouredwasp <48547220+Armouredwasp@users.noreply.github.com>
2019-10-03 22:09:50 +0900

    ever

--- src/index.js
+++ src/index.js
@@ -2,8 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import * as serviceWorker from './serviceWorker';
+import Cardlist from './cardlist.js';
+import 'tachyons';
+import { robots } from './robots';
+import App from './app.js'
 
-ReactDOM.render(<p>I will kill you</p>, document.getElementById('root'));
+
+ReactDOM.render(
+            <div>
+                    <App/>
+            </div>                
+                
+                
+                
+                , document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
